e3faaa68bcc504c9502355ccfe1d591d18539f5a
sakhiyap <sakhiyap@sheridancollege.ca>
2022-05-25 14:12:55 -0400

    Version 1

---
+++ src/studentarray/Student.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentarray;
+/**
+ * model class - raw data for student template
+ * @author pratik sakhiya
+ */
+public class Student 
+{
+    private String name;
+    private int age = 40;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString(){
+        return "Name: "+getName()+"\tAge: "+getAge();
+    }
+}  
+
+   
---
+++ src/studentarray/StudentArray.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentarray;
+
+/**
+ *
+ * @author pratik sakhiya
+ */
+public class StudentArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //create student object
+        Student[] studentlist = new Student[3];  // Array of Object
+        
+        Student s1 = new Student();
+        s1.setName("John");
+        s1.setAge(19);
+        
+        Student s2 = new Student();
+        s2.setName("Utsav");
+        s2.setAge(20);
+        
+        Student s3 = new Student();
+        s3.setName("Jaysheel");
+        s3.setAge(25);
+       
+        studentlist[0]=s1;
+        studentlist[1]=s2;
+        studentlist[2]=s3;
+        
+        for(int i=0;i<studentlist.length;i++)
+        {
+            System.out.println(studentlist[i]);
+        }    
+        
+        
+        
+    }
+    
+}
